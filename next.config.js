edb23c7b8c62a6ca0ed657306dc8fa49aed8676d
puercalvaro <64426866+puercalvaro@users.noreply.github.com>
2022-09-13 17:58:45 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,12 @@ module.exports = (phase, { defaultConfig }) => {
       ],
     },
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+          'util/types': false
+        }
+    }
       config.resolve.alias = {
         ...config.resolve.alias,
         './runtimeConfig': './runtimeConfig.browser',
