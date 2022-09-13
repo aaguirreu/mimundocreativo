ead764ac9e5526c93f85c0510e43c3dc1909ee8e
puercalvaro <64426866+puercalvaro@users.noreply.github.com>
2022-09-13 19:54:32 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = (phase, { defaultConfig }) => {
       if (!isServer) {
         // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
         config.resolve.fallback = {
-          util: false,
+          util: require.resolve("util/"),
           process: false,
           buffer: false
         }
