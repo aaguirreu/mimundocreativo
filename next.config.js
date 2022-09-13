5d63cf6061b3fb29d1ce3ed996fe6b65157a023b
puercalvaro <64426866+puercalvaro@users.noreply.github.com>
2022-09-13 18:06:05 -0300

    webpack lastest

--- next.config.js
+++ next.config.js
@@ -10,19 +10,6 @@ module.exports = (phase, { defaultConfig }) => {
         "kagjkwomsitfugyxfvlj.supabase.co",
       ],
     },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      if (!isServer) {
-        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-        config.resolve.fallback = {
-          'util/types': false
-        }
-    }
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        './runtimeConfig': './runtimeConfig.browser',
-      }
-      return config
-    },
 }
   return nextConfig
 }
