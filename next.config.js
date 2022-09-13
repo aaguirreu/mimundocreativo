066e83a8b1a45315f00537a6c28f4ffdd018c1e0
puercalvaro <64426866+puercalvaro@users.noreply.github.com>
2022-09-13 16:28:37 -0300

    deprecated target next-config.js fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true,
+    images: {
     domains: [
       "kagjkwomsitfugyxfvlj.supabase.in",
       "kagjkwomsitfugyxfvlj.supabase.co",
     ],
   },
-};
+  }
+  return nextConfig
+}
